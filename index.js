d457e885287b62f51271f6c6777010d107a5513f
iqramahamed <iqra.mahamed64@gmail.com>
2024-04-17 16:07:36 +0300

    last commit

--- index.js
+++ index.js
@@ -20,4 +20,61 @@ const allWagesFor = function () {
 
     return payable
 }
+function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+}
+function createEmployeeRecord(arr) {
+    return {
+       firstName: arr[0],
+       familyName: arr[1],
+       title: arr[2],
+       payPerHour: arr[3],
+       timeInEvents: [],
+       timeOutEvents: []
+    };
+}
+
+
+function createTimeInEvent (dayTime) {
+    const[date, hour] = dayTime.split(' ');
+    this.timeInEvents.push({ type: 'TimeIn', date, hour: parseInt(hour, 10)});
+    return this;
+}
+
+function createTimeOutEvent (dayTime) {
+    if (!this) {
+        throw new Error("Employee record is undefined");
+    }
+    this.timeOutEvents = this.timeOutEvents || [];
+    const [date, hour] = dayTime.split(' ');
+    this.timeOutEvents.push({type: 'TimeOut', date, hour:parseInt(hour, 10)});
+    return this;
+}
+
+
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+
+    if(!timeIn || !timeOut) {
+        return 0;
+    }
+    const startTime = parseInt(timeIn.hour, 10);
+    const endTime = parseInt(timeOut.hour, 10);
+    return(endTime - startTime) / 100;
+   
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWworked = hoursWorkedOnDate.call(this, date);
+    return hoursWworked * this.payPerHour
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((total, employee) => total + allWagesFor.call(employee), 0)
+}
+function findEmployeeByFirstName(collection, firstNameString) {
+    return collection.find(employee => employee.firstName === firstNameString);
+}
 
